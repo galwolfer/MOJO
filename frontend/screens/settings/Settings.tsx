a9015d05a9cede427b1caa0b1a5c2a3db9d9189d
gal wolfer <galwolfer@gmail.com>
2026-02-04 06:37:47 +0900

    documentation

--- frontend/screens/settings/Settings.tsx
+++ frontend/screens/settings/Settings.tsx
@@ -21,7 +21,6 @@ import { useAuth } from "../../context/AuthContext";
  * - Profile settings section with user info
  * - My Preferences section with various settings options
  * - Sign out button
- * hi
  */
 
 type SettingsScreenProps = {
